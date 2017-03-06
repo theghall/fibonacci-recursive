12449a025e5c0a667d22ba9771b7347ab397eeb4
theghall <guategary@gmail.com>
2017-03-06 13:26:15 -0500

    Add recursive solution

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,7 +3,6 @@
 # 20170306	GH
 #
 # Find first n numbers in the fibonnaci sequence iteratively
-require 'byebug'
 def fibs(n)
   fib_array = [0, 1]
 
@@ -14,23 +13,26 @@ def fibs(n)
 end
 
 # Find first n number in the fibonacci sequence recursively
-def fibs_rec(num, acc = 0, ary = [])
-  return num if num <= 1
-  acc = fibs_rec(num - 1, acc, ary) + fibs_rec(num - 2, acc, ary)
+def fibs_rec(num)
+  return 0 if num == 0
+  return 1 if num == 1
+  fibs_rec(num - 1) + fibs_rec(num - 2)
 end
 
 puts("Iteratively determined Fibonacci numbers:")
 
-[0,1,2,7].each do |n|
-  puts("The first #{n + 1} fibonacci number(s):")
+[1, 3, 8].each do |n|
+  puts("The first #{n} fibonacci number(s):")
 
-  puts("=> #{fibs(n)}")
+  puts("=> #{fibs(n - 1)}")
 end
 
-puts("Recusivively determined Fibonacci numbers:")
+puts("\n\nRecusivively determined Fibonacci numbers:")
 
-[7].each do |n|
-  puts("The first #{n + 1} fibonacci number(s):")
+[1, 3, 8].each do |n|
+  puts("The first #{n} fibonacci number(s):")
 
-  puts("=> #{fibs_rec(n)}")
+  ary = []
+  n.times {|n| ary << fibs_rec(n) }
+  p ary
 end

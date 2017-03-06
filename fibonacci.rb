c71e1f8847adc767ebde0c164614310f887e9249
theghall <guategary@gmail.com>
2017-03-06 10:17:32 -0500

    Refactor fibs, add fibs_rec

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,21 +3,34 @@
 # 20170306	GH
 #
 # Find first n numbers in the fibonnaci sequence iteratively
+require 'byebug'
 def fibs(n)
-  fib_array = []
+  fib_array = [0, 1]
 
-  (0..n - 1).each do |i|
-    if i == 0
-      fib_array << 0
-    elsif i == 1
-      fib_array << 1
-    else
+  (2..n).each do |i|
       fib_array << fib_array[i - 1] + fib_array[i - 2]
-    end
   end
-  fib_array
+  fib_array[0..n]
 end
 
-p fibs(1)
-p fibs(2)
-p fibs(8)
+# Find first n number in the fibonacci sequence recursively
+def fibs_rec(num, acc = 0, ary = [])
+  return num if num <= 1
+  acc = fibs_rec(num - 1, acc, ary) + fibs_rec(num - 2, acc, ary)
+end
+
+puts("Iteratively determined Fibonacci numbers:")
+
+[0,1,2,7].each do |n|
+  puts("The first #{n + 1} fibonacci number(s):")
+
+  puts("=> #{fibs(n)}")
+end
+
+puts("Recusivively determined Fibonacci numbers:")
+
+[7].each do |n|
+  puts("The first #{n + 1} fibonacci number(s):")
+
+  puts("=> #{fibs_rec(n)}")
+end
